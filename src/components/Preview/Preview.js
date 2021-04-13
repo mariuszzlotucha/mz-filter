1a7f4b38d48103501f7c0a9d2e30e14743f9e95a
Mariusz Złotucha <mariusz.zlotucha@gmail.com>
2021-04-13 23:14:59 +0200

    WIP

--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { useRootStore } from '../../contexts';
 import './style.sass';
 import { observer } from 'mobx-react-lite';
-
+import { fabric } from 'fabric';
+import { useFabric } from './useFabric';
 export const Preview = observer(() => {
     const { imageStore } = useRootStore();
     const { imagePath } = imageStore;
-    return imagePath && <img src={imagePath} alt={imagePath} />;
+
+    const ref = useFabric((fabricCanvas) => {
+        fabricCanvas.setBackgroundImage(
+            new fabric.Image.fromURL(imagePath, () => {}, {
+                left: 0,
+                top: 0,
+                width: 150,
+                height: 150,
+            })
+        );
+        fabricCanvas.renderAll.bind(fabricCanvas);
+    });
+
+    return (
+        <div style={{ border: '1px solid red' }}>
+            <canvas ref={ref} width={300} height={200} />
+        </div>
+    );
 });
---
+++ src/components/Preview/useFabric.js
@@ -0,0 +1,21 @@
+import { useRef, useCallback } from 'react';
+import { fabric } from 'fabric';
+
+export const useFabric = (onChange) => {
+    const fabricRef = useRef();
+    const disposeRef = useRef();
+    return useCallback((node) => {
+        if (node) {
+            fabricRef.current = new fabric.StaticCanvas(node);
+            if (onChange) {
+                disposeRef.current = onChange(fabricRef.current);
+            }
+        } else if (fabricRef.current) {
+            fabricRef.current.dispose();
+            if (disposeRef.current) {
+                disposeRef.current();
+                disposeRef.current = undefined;
+            }
+        }
+    }, []);
+};
